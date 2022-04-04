2e0baeaf17c03b099314851f69f4f138af8b3a32
Anthony <anthonypoullain.pro@gmail.com>
2022-04-04 18:02:36 +0700

    homepage data + styling - data shared with photographer.html through id in url

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,11 @@
-//Mettre le code JavaScript lié à la page photographer.html
+// Mettre le code JavaScript lié à la page photographer.html
+let params = new URL(document.location).searchParams;
+let id = parseInt(params.get('id'));
+
+// getting the photographers array from sessionStorage
+const photographers = JSON.parse(
+  window.sessionStorage.getItem('photographers')
+);
+
+const photographer = photographers.find((item) => item.id === id);
+console.log(photographer);
